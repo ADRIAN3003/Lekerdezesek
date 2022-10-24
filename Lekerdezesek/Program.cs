919b6c2186af3cd8bbe46739f233d91176252c66
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-24 14:00:57 +0200

    Add project files.

---
+++ Lekerdezesek/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekerdezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szamok = { 4, 5, 1, 2, 8, 9, 7, 3 };
+
+            string[] nums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var kicsi = (from szam in szamok
+                        where szam >= 2 && szam <= 6
+                        orderby szam descending
+                        select nums[szam]).ToList();
+
+            foreach (var k in kicsi)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
